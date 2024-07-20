f6a1ddce0f645c323b787e470249eeb0ef7be79b
Vaibhav <vaibhav.pandey0806@gmail.com>
2024-07-20 12:18:26 +0530

    feat: initialized cli scaffold

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,28 @@
 const figlet = require("figlet");
+const { Command } = require("commander");
 
 console.log(figlet.textSync("Reminders"));
+
+const program = new Command();
+
+program
+  .version("1.0.0")
+  .description("An example CLI for managing a directory")
+  .option("-a, --add  <value>", "add a task")
+  .option("-l, --list", "list all tasks")
+  .option("-r, --remove <value>", "remove a task")
+  .parse(process.argv);
+
+const options = program.opts();
+
+if (options.add) {
+    console.log('adding a new task...', options.add);
+}
+
+if (options.list) {
+    console.log('listing all tasks...', options.list);
+}
+
+if (options.remove) {
+    console.log('removing a task...', options.remove);
+}
